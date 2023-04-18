1ac7534d78bfb48b3fdd9d9317b242292ddf88d6
Dwlad <dwlad90@gmail.com>
2023-04-18 15:39:04 +0000

    Update 5 files

--- src/app/[...segments]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface SectionPageProps {
-  params: { sectionNames: string[] };
-}
-
-export default function SectionPage({ params }: SectionPageProps) {
-  const { sectionNames } = params;
-
-  return <div>Section {sectionNames.join('/')}</div>;
-}
